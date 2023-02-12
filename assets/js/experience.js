808940cd6e20a22d3f34250b75f6199fa6713da2
SrS4mmy <bryanadinugraha46@gmail.com>
2023-02-13 00:36:43 +0700

    First commit

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -52,52 +52,52 @@ document.addEventListener("DOMContentLoaded", showCards2);
 
 // Volunteership Cards
 
-// const volunteership = document.querySelector(".volunteership");
-// const volunteershipcards = [
-//   {
-//     title: "GirlScript Summer Of Code 2020",
-//     cardImage: "assets/images/experience-page/1.jpg",
-//     description:
-//       "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
-//   },
-//   {
-//     title: "StudentCode-in 2020",
-//     cardImage: "assets/images/experience-page/2.jpg",
-//     description:
-//       "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-//   },
-//   {
-//     title: "PClub Summer Of Code 2020",
-//     cardImage: "assets/images/experience-page/3.jpg",
-//     description:
-//       "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-//   },
-//   {
-//     title: "Hakin-Codes",
-//     cardImage: "assets/images/experience-page/4.jpg",
-//     description:
-//       "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
-//   },
-// ];
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "GirlScript Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/1.jpg",
+    description:
+      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+  },
+  {
+    title: "StudentCode-in 2020",
+    cardImage: "assets/images/experience-page/2.jpg",
+    description:
+      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+  },
+  {
+    title: "PClub Summer Of Code 2020",
+    cardImage: "assets/images/experience-page/3.jpg",
+    description:
+      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+  },
+  {
+    title: "Hakin-Codes",
+    cardImage: "assets/images/experience-page/4.jpg",
+    description:
+      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+  },
+];
 
-// const showCards = () => {
-//   let output = "";
-//   volunteershipcards.forEach(
-//     ({ title, cardImage, description }) =>
-//       (output += `        
-//       <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
       
-//       <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
-//       <div class="content">
-//           <h2 class="volunteerTitle">${title}</h2><br>
-//           <p class="copy">${description}</p></div>
+      <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
       
-//       </div>
-//       `)
-//   );
-//   volunteership.innerHTML = output;
-// };
-// document.addEventListener("DOMContentLoaded", showCards);
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
 
 // // Hackathon Section
 
@@ -210,25 +210,25 @@ document.addEventListener("DOMContentLoaded", showCards2);
 //   },
 // ];
 
-// const showCards3 = () => {
-//   let output = "";
-//   mentor.forEach(
-//     ({ title, image, subtitle, desp, href }) =>
-//       (output += `  
-//       <div class="blog-slider__item swiper-slide">
-//         <div class="blog-slider__img">
-//             <img src="${image}" alt="">
-//         </div>
-//         <div class="blog-slider__content">
-//           <div class="blog-slider__title">${title}</div>
-//           <span class="blog-slider__code">${subtitle}</span>
-//           <div class="blog-slider__text">${desp}</div>
-//           <a href="${href}" class="blog-slider__button">Read More</a>   
-//         </div>
-//       </div>
-//       `)
-//   );
-//   hackathonsection.innerHTML = output;
-// };
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp, href }) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="${href}" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
 
-// document.addEventListener("DOMContentLoaded", showCards3);
+document.addEventListener("DOMContentLoaded", showCards3);
